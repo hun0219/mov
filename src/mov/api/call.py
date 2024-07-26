3e931fc924e3f85fda539dc76ff550d2c94684c9
hun0219 <gsw98000@gmail.com>
2024-07-26 14:07:12 +0900

    call efit

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,15 +1,20 @@
 import requests
+import json
 
 def req(dt="20120101"):
+    #url = gen_url('20240720')
+    url = gen_url(dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    print(data)
+    return code, data
+
+def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = "82d0fee233046d8dce1be7c27453df1b"
+    #key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
     url = f"{base_url}?key={key}&targetDt={dt}"
 
-    return req
+    return url
 
-#req()
-#req("8" * 8)
-#req(dt="ABCDE")
-
-def req():
-    print("req")
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,16 @@
-from mov.api.call import gen_url
+from mov.api.call import gen_url, req
 
 def test_gen_url():
     url = gen_url()
-    assert True
+    #assert True
     assert "http" in url
     assert "kobis" in url
+
+def test_req():
+    code, data = req()
+    assert code == 200
+
+    #assert req('20240710') == 200
+    
+    code, data = req('20230101')
+    assert code == 200
